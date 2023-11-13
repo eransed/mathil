87277d300fc703aa586f806ee1f73e0abd673f02
eransed <emil.ransed@gmail.com>
2023-11-13 18:01:03 +0100

    add manual lib/package info accessible with runtime functions

---
+++ src/_package_info.ts
@@ -0,0 +1,17 @@
+import { mathil_info } from "./_version";
+
+export function getVersionInfo() {
+    return mathil_info
+}
+
+// export function getFullVersion(): string {
+//     return `${mathil_info.name} ${mathil_info.version} (${mathil_info.build_date_gmt})`
+// }
+
+export function getNameVersion(): string {
+    return `${mathil_info.name} ${mathil_info.version}`
+}
+
+export function getVersion(): string {
+    return `${mathil_info.version}`
+}
--- tests/test.ts
+++ tests/test.ts
@@ -3,7 +3,9 @@ import { runColorTests } from "./test_color";
 import { run_log_tests } from "./test_log";
 import { basic_matrixMul, basic_newMat } from "./test_matrix";
 import { runDist2Tests, runVec2Tests, runVec3Tests } from "./test_vec2";
+import { getVersion } from "../src/_package_info"
 info(`Running tests, t=${dateTimeStamp()}`)
+info(`Version: ${getVersion()}`)
 runVec2Tests()
 runVec3Tests()
 runDist2Tests()
